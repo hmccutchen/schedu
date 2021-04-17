ecfe93f3fdbf3e697d82b0fc9de58bb492386370
Hassan McCutchen <mccutchen.hassan@gmail.com>
2021-04-17 18:22:50 +0000

    add user auth

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   get 'about', to: "about#index"
   get 'sign_up', to: "registrations#new"
   post "sign_up", to: "registrations#create"
+
+  get 'log_in', to: "sessions#new"
+  post "log_in", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
-  
+
   root to: "main#index"
 end
