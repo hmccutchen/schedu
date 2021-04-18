e279b54fafb55de92d61cb158e4deb008944ea22
Hassan McCutchen <mccutchen.hassan@gmail.com>
2021-04-18 16:47:17 +0000

    add password reset feature

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -10,5 +10,28 @@ class PasswordResetsController < ApplicationController
    PasswordMailer.with(user: @user).reset.deliver_now
   end
   redirect_to root_path, notice: "If an account with that email was found, we have sent a link to reset your password."
+end
+
+def edit
+  @user = User.find_signed(params[:token], purpose: "password_reset")
+  rescue ActiveSupport::MessageVerifier::InvalidSignature
+    redirect_to log_in_path, alert: "Your token has expired please try again"
+  end
+
+  def update
+    @user = User.find_signed(params[:token], purpose: "password_reset")
+
+    if @user.update(password_params)
+      redirect_to log_in_path, notice: "Your password was reset successfully, please log in."
+    else
+    render :edit
+    end
+  end
+   
+  private
+
+  def password_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
 
   get "password/reset", to: "password_resets#new"
   post "password/reset", to: "password_resets#create"
+
+  get "password/reset/edit", to: "password_resets#edit"
+  patch "password/reset/edit", to: "password_resets#update"
   get 'log_in', to: "sessions#new"
   post "log_in", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
