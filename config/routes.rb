af24eb8f2c2412634e2a56c3a6268b4761264d23
Hassan McCutchen <mccutchen.hassan@gmail.com>
2021-04-18 13:33:22 +0000

    setup forgot password mailer

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   get 'about', to: "about#index"
   get 'sign_up', to: "registrations#new"
   post "sign_up", to: "registrations#create"
+ 
+  get "password", to: "passwords#edit", as: :edit_password
+  patch "password", to: "passwords#update"
 
+  get "password/reset", to: "password_resets#new"
+  post "password/reset", to: "password_resets#create"
   get 'log_in', to: "sessions#new"
   post "log_in", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
