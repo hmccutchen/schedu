e0c4d4befc346af39d9a933b0b68ec0ecc1287dc
hmccutchen <mccutchen.hassan@gmail.com>
2021-04-20 00:54:34 -0400

    allow user to connect twitter account

--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,11 @@ Rails.application.routes.draw do
   post "log_in", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
 
+  get "auth/twitter/callback", to: "omniauth_callbacks#twitter"
+  
+  resources :twitter_accounts 
+  
+
+
   root to: "main#index"
 end
